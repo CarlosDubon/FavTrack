0a4becab23b4ef1f609cbb4b67328c5085db5660
CarlosDubon <00058016@uca.edu.sv>
2018-07-10 07:31:22 -0600

    Initial commit

---
+++ app/src/main/java/com/bonusteam/favtrack/FavTrack.java
@@ -0,0 +1,13 @@
+package com.bonusteam.favtrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavTrack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fav_track);
+    }
+}
