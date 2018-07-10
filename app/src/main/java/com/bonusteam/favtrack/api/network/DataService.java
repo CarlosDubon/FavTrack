c9d5b21a43aaa1aa1784e09b336f4b34a8803d1d
RDR96 <00231514@uca.edu.sv>
2018-07-10 16:47:51 -0600

    Se modificaron los repositorios

--- app/src/main/java/com/bonusteam/favtrack/api/network/DataService.java
+++ app/src/main/java/com/bonusteam/favtrack/api/network/DataService.java
@@ -1,8 +1,14 @@
 package com.bonusteam.favtrack.api.network;
 
+import com.bonusteam.favtrack.room.pojos.Dieta;
+import com.bonusteam.favtrack.room.pojos.Rutina;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 /**
@@ -13,5 +19,11 @@ public interface DataService {
     @FormUrlEncoded
     @POST("/login")
     Call<String> login(@Field("username") String user, @Field("password")String password);
+
+    @GET("/rutinas")
+    Call<List<Rutina>> getRutinas();
+
+    @GET("/diets")
+    Call<List<Dieta>> getDiets();
 
 }
