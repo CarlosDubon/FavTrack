40ed1ea115f01da7bc13878510bb5adc87a6d1e9
Didier Cruz <didier.cruz98@gmail.com>
2018-07-10 16:04:01 -0600

    cambiando

---
+++ app/src/main/java/com/bonusteam/favtrack/fragments/ViewPagerFragment.java
@@ -0,0 +1,19 @@
+package com.bonusteam.favtrack.fragments;
+
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+/**
+ * Created by Deryan Cruz on 10/07/2018.
+ */
+
+public class ViewPagerFragment extends Fragment {
+    View vista;
+    private AppBarLayout appBar;
+    private TabLayout pestanas;
+    private ViewPager viewPager;
+    private static String dato;
+}
