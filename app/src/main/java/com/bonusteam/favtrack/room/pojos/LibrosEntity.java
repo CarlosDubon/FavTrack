ead730685aa9d87dd399794d7aad442501b0b11f
uca <uca>
2018-07-10 12:31:48 -0600

    agregando LibrosEntity/Dao

---
+++ app/src/main/java/com/bonusteam/favtrack/room/pojos/LibrosEntity.java
@@ -0,0 +1,84 @@
+package com.bonusteam.favtrack.room.POJOS;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity
+public class LibrosEntity {
+
+    @NonNull
+    @PrimaryKey
+    @ColumnInfo(name = "id_Libro")
+    private String idLibro;
+
+    private String descripcion;
+
+    private String genero;
+
+    private String autor;
+
+    private String editorial;
+
+    private String anio;
+
+    private String avatar;
+
+    @NonNull
+    public String getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(@NonNull String idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
